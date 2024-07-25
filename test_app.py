ff466717e7a37f29733a81df6ca7caef7c343236
William Davis <william.davis@missionsupport.us>
2024-07-25 14:01:09 -0600

    working database connection test

--- test_app.py
+++ test_app.py
@@ -2,23 +2,28 @@
 
 import unittest
 from app import app, db
+from sqlalchemy.sql import text
 
 class DataBaseConnectionTestCase(unittest.TestCase):
 
     def setUp(self):
+        print('DATABASE SETUP STARTED')
         app.config['TESTING'] = True
-        app.config['SQLALCHEMY_DATABASE_URI'] = ''
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:Msi_123@localhost:5432/portal_data_base'
         self.app = app.test_client()
-        db.create_all()
+
+        with app.app_context():
+            db.create_all()
 
     def tearDown(self):
-        db.session.remove()
-        db.drop_all()
+        with app.app_context():
+            db.session.remove()
+            db.drop_all()
 
     def test_db_connect(self):
         """Test database connection"""
         with app.app_context():
-            result = db.session.execute('SELECT 1')
+            result = db.session.execute(text('SELECT 1'))
             self.assertEqual(result.fetchone()[0], 1)
 
 if __name__ == '__main__':
