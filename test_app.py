bb4e7f8b2d5c830f28ba9449fc5d36477238625a
William Davis <william.davis@missionsupport.us>
2024-07-26 10:19:46 -0600

    updated test added with tables

--- test_app.py
+++ test_app.py
@@ -3,14 +3,18 @@
 import unittest
 from app import app, db
 from sqlalchemy.sql import text
+from sqlalchemy import inspect, create_engine
+from ignore_DO_NOT_UPLOAD import db_url, expected_tables
 
 class DataBaseConnectionTestCase(unittest.TestCase):
 
     def setUp(self):
         print('DATABASE SETUP STARTED')
         app.config['TESTING'] = True
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:Msi_123@localhost:5432/portal_data_base'
+        app.config['SQLALCHEMY_DATABASE_URI'] = db_url
         self.app = app.test_client()
+        self.engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+        self.inspector = inspect(self.engine)
 
         with app.app_context():
             db.create_all()
@@ -25,6 +29,13 @@ class DataBaseConnectionTestCase(unittest.TestCase):
         with app.app_context():
             result = db.session.execute(text('SELECT 1'))
             self.assertEqual(result.fetchone()[0], 1)
+
+    def test_tables_exist(self):
+        with app.app_context():
+            # Get list of tables from database
+            actual_tables = self.inspector.get_table_names()
+            for table in expected_tables:
+                self.assertIn(table, actual_tables, f"Table {table} does not exist in the database")
 
 if __name__ == '__main__':
     unittest.main()
